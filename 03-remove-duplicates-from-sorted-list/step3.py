8cd0839ced2edc6c7eabfc49e8494ee764f2c839
Kota-Isayama <isakou.2208@icloud.com>
2025-09-15 01:02:09 +0900

    Revert "Add side effective answers."
    This reverts commit fcf12fa5925ff902a2a9c90f1ee6010b7e25dfbb.

--- 03-remove-duplicates-from-sorted-list/step3.py
+++ 03-remove-duplicates-from-sorted-list/step3.py
@@ -1,5 +1,5 @@
 # Step3の感想
-# 頭の中でrun_begin_nodeとnodeがシャクトリムシのようにリストを走査していくところを想像しながら書いた。
+# 頭の中でbeginとendがシャクトリムシのようにリストを走査していくところを想像しながら書いた。
 # 3回連続で3分以内に書き切れたので一区切りとする。
 
 from typing import Optional
@@ -21,42 +21,14 @@ class Solution:
         deduplicated_list_head = ListNode(head.val, None)
         deduplicated_list_tail = deduplicated_list_head
 
-        run_begin_node = head
-        while run_begin_node is not None:
-            node = run_begin_node
-            while node is not None and run_begin_node.val == node.val:
-                node = node.next
+        begin = head
+        end = head
+        while begin is not None:
+            while end is not None and begin.val == end.val:
+                end = end.next
 
-            deduplicated_list_tail.next = ListNode(node.val) if node is not None else None
+            deduplicated_list_tail.next = ListNode(end.val, None) if end is not None else None
             deduplicated_list_tail = deduplicated_list_tail.next
-            run_begin_node = node
+            begin = end
 
         return deduplicated_list_head
-
-    def deleteDuplicatesWithSideEffect1(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # When the list is empty.
-        if head is None:
-            return None
-
-        node = head
-        while node is not None:
-            if node.next is not None and node.val == node.next.val:
-                node.next = node.next.next
-            else:
-                node = node.next
-
-        return head
-
-    def deleteDuplicatesWithSideEffect2(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # When the list is empty.
-        if head is None:
-            return None
-
-        node = head
-        while node is not None:
-            while node.next is not None and node.val == node.next.val:
-                node.next = node.next.next
-
-            node = node.next
-
-        return head
